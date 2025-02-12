e898a1d6984c6b643f11b3f9207ba6326ed21261
Simon <siga241@gmail.com>
2025-02-12 13:23:54 +0100

    [ #2 ] Potential fix

--- addons/SimonsStateChart/StateTransition/transition_queue_item.gd
+++ addons/SimonsStateChart/StateTransition/transition_queue_item.gd
@@ -4,18 +4,17 @@ class_name TransitionQueueItem
 signal transition_ready(transition:StateTransition)
 
 var transition:StateTransition
-var timer:Timer
+var timer:Timer = Timer.new()
 
 func _init(new_transition):
 	transition = new_transition
 
 func _ready():
 	if transition.delay_seconds > 0:
-		timer = Timer.new()
-		add_child(timer)
 		timer.one_shot = true
 		timer.wait_time = transition.delay_seconds
 		timer.timeout.connect(_on_timer_timeout)
+		add_child(timer)
 		timer.start()
 	else:
 		_on_timer_timeout()
